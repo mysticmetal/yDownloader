03d8c00f43e1263701b52b4d6463148281cee825
Unknown <rao.waris440@gmail.com>
2018-08-30 19:17:45 +0530

    change in structure

--- src/Downloader.js
+++ src/Downloader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {SERVER_URL} from './appConfig';
 import Playlist from './Playlist';
+import Yimage from './youtube.png';
 class Downloader extends React.Component{
   constructor(){
     super();
@@ -12,12 +13,18 @@ class Downloader extends React.Component{
       subtitle:false,
       playlist:[
         {
-          error:false,
-          vId:'jV3xxOoWe-4'
+          status:'active',
+          vId:'jV3xxOoWe-4',
+          completed:false
         },
-        { error:true,
-          vId:'M-P4QBt-FWw'
-        }  
+        { status:'error',
+          vId:'M-P4QBt-FWw',
+          completed:false
+        },
+        {
+          status:'completed',
+          vId:'hT_nvWreIhg'
+        }
       ]
     }
     this.handleChange=this.handleChange.bind(this);
@@ -56,42 +63,71 @@ class Downloader extends React.Component{
     
     if(this.state.fetched){
       
-      console.log('downloading');
-      
       let indx = this.state.playlist.map( v=> v.vId).indexOf(this.state.vId);
       let newPlaylist=[...this.state.playlist];
 
       if(indx===-1){
-        newPlaylist=[...newPlaylist,{error:false,vId:this.state.vId}];
+        console.log('downloading');
+        fetch(SERVER_URL+'/download',{
+          method:"POST",
+          mode:"cors",
+          credentials:"omit",
+          headers:{
+            "Content-type":"application/json; charset=utf-8"
+          },
+          body:JSON.stringify({
+            vId:this.state.vId,
+            vQuality:this.state.vQuality,
+            subtitle:this.state.subtitle
+          })
+        })
+        .then(res=>res.json())
+        .then(res=>{
+          console.log(res)
+          if(res.status){
+            let indx = this.state.playlist.map( v=> v.vId).indexOf(res.vInfo.vId) ;
+            let newPlaylist = [...this.state.playlist];
+            newPlaylist=[...newPlaylist.slice(0,indx),{status:'error',vId:res.vInfo.vId,...newPlaylist.slice(indx+1)}];
+            this.setState({playlist:newPlaylist});
+          }
+        });
+
+        newPlaylist=[...newPlaylist,{status:'active',vId:this.state.vId}];
+      
       }else{
-        newPlaylist=[...newPlaylist.slice(0,indx),{error:false,vId:this.state.vId},...newPlaylist.slice(indx+1)];
+        if(newPlaylist[indx].status){
+          
+          console.log('downloading');
+          fetch(SERVER_URL+'/download',{
+            method:"POST",
+            mode:"cors",
+            credentials:"omit",
+            headers:{
+              "Content-type":"application/json; charset=utf-8"
+            },
+            body:JSON.stringify({
+              vId:this.state.vId,
+              vQuality:this.state.vQuality,
+              subtitle:this.state.subtitle
+            })
+          })
+          .then(res=>res.json())
+          .then(res=>{
+            console.log(res)
+            if(res.status){
+              let indx = this.state.playlist.map( v=> v.vId).indexOf(res.vInfo.vId) ;
+              let newPlaylist = [...this.state.playlist];
+              newPlaylist=[...newPlaylist.slice(0,indx),{status:'error',vId:res.vInfo.vId,...newPlaylist.slice(indx+1)}];
+              this.setState({playlist:newPlaylist});
+            }
+          });  
+        
+          newPlaylist=[...newPlaylist.slice(0,indx),{status:'active',vId:this.state.vId},...newPlaylist.slice(indx+1)];
+        }
+      
       }
       
       this.setState({fetched:false,playlist:newPlaylist});
-      
-      fetch(SERVER_URL+'/download',{
-        method:"POST",
-        mode:"cors",
-        credentials:"omit",
-        headers:{
-          "Content-type":"application/json; charset=utf-8"
-        },
-        body:JSON.stringify({
-          vId:this.state.vId,
-          vQuality:this.state.vQuality,
-          subtitle:this.state.subtitle
-        })
-      })
-      .then(res=>res.json())
-      .then(res=>{
-        console.log(res)
-        if(res.error){
-          let indx = this.state.playlist.map( v=> v.vId).indexOf(res.vInfo.vId) ;
-          let newPlaylist = [...this.state.playlist];
-          newPlaylist=[...newPlaylist.slice(0,indx),{error:true,vId:res.vInfo.vId,...newPlaylist.slice(indx+1)}];
-          this.setState({playlist:newPlaylist});
-        }
-      });
     
     }
 
@@ -99,33 +135,50 @@ class Downloader extends React.Component{
 
   render(){
     return(
-      <div>
+      <div className="downloader downloaderLayout">
+       
+        <div className="header">
         <h1 className="heading" >Ydownloader for downloading videos from youtube</h1>
-        <input className="urlInput" name="urlInput" onChange={this.handleChange} placeholder="enter the url of video" type="url" value={this.state.url} /> 
-        
-        { (!!(this.state.ulr)) && 
-          <button className="btn btnClear" onClick={this.handleChange} name="clearBtn" > Clear </button>
-        }
-        
-        <button className="btn btnFetch" onClick={this.handleChange} name="fetchBtn" > Fetch </button>
-        {
-          this.state.vId &&
-          <iframe width="420" height="315" src={ "https://www.youtube.com/embed/"+ this.state.vId}></iframe>
-        }
-        
-        
-        <label htmlFor="qualitySelect" >Select Quality</label>
-        <select name="videoQuality" id="qualitySelect" defaultValue="medium"  onChange={this.handleChange} >
-          <option value="audio" >Audio Only</option> 
-          <option value="low">Low</option>
-          <option value="medium" >Medium</option>
-          <option value="high">High</option>
-        </select>
-        
-        <label htmlFor="subInput" >With Subtitle</label>
-        <input type="checkbox" id="subInput" name="subInput" onChange={this.handleChange} />
+        </div>
 
-        <button className="btn btnDownload" onClick={this.onDownload} name="downloadBtn" > Download </button>
+        <div className="searchBar" >  
+          <input className="urlInput" name="urlInput" onChange={this.handleChange} placeholder="enter the url of video" type="url" value={this.state.url} /> 
+          <button className="btn btnFetch" onClick={this.handleChange} name="fetchBtn" > Fetch </button>
+        </div>
+        
+        <div className="videoContainer" >
+        {
+          !(this.state.fetched) && !(this.state.vId) &&
+          <img className="imagePreview" width="420" height="315" alt="video preview"
+          src={Yimage} />
+        }
+        {  this.state.vId &&
+          <iframe className="videoPreview" width="420" height="315" src={ "https://www.youtube.com/embed/"+ this.state.vId}></iframe>
+        }
+        </div>
+        
+        <div className="videoAttributes" >
+        
+          <div className="videoQualitySelector">
+            <label className="labelText" htmlFor="qualitySelect" >Select Quality</label>
+            <select className="videoQuality" name="videoQuality" id="qualitySelect" defaultValue="medium"  onChange={this.handleChange} >
+              <option value="audio" >Audio Only</option> 
+              <option value="low">Low</option>
+              <option value="medium" >Medium</option>
+              <option value="high">High</option>
+            </select>
+          </div>
+        
+          <div className="subtitleSelector"> 
+            <label className="labelText" htmlFor="subInput" >With Subtitle</label>
+            <input className="subtitleCheckbox" type="checkbox" id="subInput" name="subInput" onChange={this.handleChange} />
+          </div>
+
+        </div>
+        
+        <div className="videoDownload">
+          <button className="btn btnDownload" onClick={this.onDownload} name="downloadBtn" > Download </button>
+        </div>
 
         <Playlist state={this.state.playlist} />
 
--- src/Playlist.js
+++ src/Playlist.js
@@ -3,17 +3,54 @@ import './Playlist.css';
 
 const  Playlist =(props)=>{
   return(
-    <div>
-      <h1 className="heading" >Videos list</h1>
-      { props.state.map((element,index) =>{
-          if(element.error){
-            return <h3 className="errorVideo" key={index}> {element.vId}</h3>
-          }else{
-            return <h3 key={index}> {element.vId}</h3>
+    <div className="videoPlaylistHolder">
+      
+      <div className="playlistHeader">
+        <h1 className="heading" >Videos list</h1>
+      </div>
+
+      <div className="videoPlaylist" >
+          { props.state.map((element,index) =>{
+              if(element.status==='error'){
+                return (
+                    <div className="videoDescription">
+                      <div className="downloadStatus" >
+                        <h3 className="arrow arrowRed " > => </h3> 
+                      </div>
+                      <div className="videoTitle" >
+                        <h3 className="videoTitleText" > {element.vId}</h3>
+                      </div>
+                    </div>
+                  );
+              }else{
+                if(element.status==='active'){
+                  return (
+                    <div className="videoDescription">
+                      <div className="downloadStatus" >
+                        <h3 className="arrow arrowYellow " > => </h3> 
+                      </div>
+                      <div className="videoTitle" >
+                        <h3 className="videoTitleText" > {element.vId}</h3>
+                      </div>
+                    </div>
+                  );
+                }else{
+                  return (
+                    <div className="videoDescription">
+                      <div className="downloadStatus" >
+                        <h3 className="arrow arrowGreen " > => </h3> 
+                      </div>
+                      <div className="videoTitle" >
+                        <h3 className="videoTitleText" > {element.vId}</h3>
+                      </div>
+                    </div>
+                  );
+                }
+              }}
+            )
           }
-          
-        })
-      }
+      </div>
+
     </div>
   );
 }
